0e88e4141155649cd49840ef2dc8c471ced82871
jesandante007 <jesandante007@gmail.com>
2023-05-02 17:19:34 +0600

    star rating & favorite button added

--- src/components/cards/RecipeCard.jsx
+++ src/components/cards/RecipeCard.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const RecipeCard = ({ recipe }) => {
+  const [disable, setDisable] = useState(false);
   const { recipeName, ingredients, cookingMethod, rating } = recipe;
+
   return (
     <div className="card bg-base-200 shadow-xl">
       <div className="card-body">
         <h2 className="card-title text-2xl mb-10">{recipeName}</h2>
-        <p><span className="font-medium">Ingredients:</span> {ingredients}</p>
-        <p><span className="font-medium">Cooking-Method: </span>{cookingMethod}</p>
+        <p>
+          <span className="font-medium">Ingredients:</span> {ingredients}
+        </p>
+        <p>
+          <span className="font-medium">Cooking-Method: </span>
+          {cookingMethod}
+        </p>
         <div className="card-actions justify-between items-center mt-10">
-          <p>Rating: {rating}</p>
-          <button className="btn btn-info normal-case text-base">Favorite</button>
+          <div className="flex gap-1">
+            <p>Rating: </p>
+            <Rating style={{ maxWidth: 100 }} value={rating} readOnly />
+            {rating}
+          </div>
+          <button
+            onClick={() => setDisable(true)}
+            className="btn btn-info normal-case text-base"
+            disabled={disable}
+          >
+            Favorite
+          </button>
         </div>
       </div>
     </div>
